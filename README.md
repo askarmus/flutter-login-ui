558a1393f59bba92c3fe04963f157819f95e1e10
Askar Musthaffa <askarmus@hotmail.com>
2020-09-20 12:55:17 +0530

    readme update

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# ui_login
+# Building simple and Beautiful login UI in Flutter
 
 A new Flutter project.
 
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+Step in Flutter and build a nice and  clean UI login
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![alt text](http://url/to/img.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shape_of_view/shape_of_view.dart';
 
 void main() {
   runApp(LoginScreen());
@@ -11,7 +12,82 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Text("Hello"),
+        backgroundColor: Color(0xFFEDF3F8),
+        body: ListView(
+          children: [
+            ShapeOfView(
+                shape: ArcShape(height: 60, position: ArcPosition.Bottom),
+                child: Image.asset("assets/images/login-top.jpg",
+                    height: 150, fit: BoxFit.cover)),
+            SizedBox(height: 40),
+            Center(
+              child: Text("Trippy",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      color: Color(0xFFF22547E))),
+            ),
+            SizedBox(height: 60),
+            Padding(
+              padding: const EdgeInsets.only(right: 40, left: 40),
+              child: RoundedInput(
+                hintText: "Email",
+              ),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.only(right: 40, left: 40),
+              child: RoundedInput(
+                hintText: "Password",
+              ),
+            ),
+            SizedBox(height: 30),
+            FlatButton(
+              onPressed: null,
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(60.0, 16.0, 60.0, 16.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xFF54B0F3),
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  child: Text("Sign In",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          color: Colors.white))),
+            ),
+            SizedBox(height: 30),
+            FlatButton(
+              onPressed: null,
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(60.0, 16.0, 60.0, 16.0),
+                  child: Text("Dont you have an account? Sign up",
+                      style:
+                          TextStyle(fontSize: 15, color: Color(0xFF54B0F3)))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoundedInput extends StatelessWidget {
+  final String hintText;
+  const RoundedInput({Key key, @required this.hintText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(50)),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(10.0),
+            hintText: hintText,
+            hintStyle: TextStyle(color: Color(0xFF54B0F3)),
+            border: InputBorder.none),
       ),
     );
   }
